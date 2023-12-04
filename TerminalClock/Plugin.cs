2d1977e85b389cd72d81d3c6264ff915f5ea266b
Jax <jaxdrummond9@gmail.com>
2023-12-04 09:51:45 -0500

    Fixed #1

--- TerminalClock/Plugin.cs
+++ TerminalClock/Plugin.cs
@@ -7,7 +7,7 @@ using static TerminalApi.Events.Events;
 
 namespace TerminalClock
 {
-	[BepInPlugin("atomic.terminalclock", "Terminal Clock", "1.0.3")]
+	[BepInPlugin("atomic.terminalclock", "Terminal Clock", "1.0.4")]
 	[BepInDependency("atomic.terminalapi", MinimumDependencyVersion: "1.2.0")]
 	public partial class Plugin : BaseUnityPlugin
 	{
@@ -26,11 +26,14 @@ namespace TerminalClock
 		private void OnTerminalAwake(object sender, TerminalEventArgs e)
 		{
 			Transform terminalMainContainer = e.Terminal.transform.parent.parent.Find("Canvas").Find("MainContainer");
-			_clock = Instantiate(terminalMainContainer.Find("CurrentCreditsNum").gameObject, terminalMainContainer);
-			_clock.name = "Clock";
-			_clock.transform.localPosition = new Vector3(255f, 200.6003f, -1.0003f);
-			_clock.transform.localScale = new Vector3(0.9f, 0.9f, 1);
-			ClockText.text = configDisplayUnkownTime.Value ? UNKOWNTIME : "";
+			if (_clock is null)
+			{
+				_clock = Instantiate(terminalMainContainer.Find("CurrentCreditsNum").gameObject, terminalMainContainer);
+				_clock.name = "Clock";
+				_clock.transform.localPosition = new Vector3(255f, 200.6003f, -1.0003f);
+				_clock.transform.localScale = new Vector3(0.9f, 0.9f, 1);
+				ClockText.text = configDisplayUnkownTime.Value ? UNKOWNTIME : "";
+			}
 		}
 	}
 }
